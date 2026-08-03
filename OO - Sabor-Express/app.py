e8b508b1cd5b0eb82f435b64f2094d061a10d8eb
João Paulo Lunkes <joaopaulolunkes@gmail.com>
2026-08-03 17:47:43 -0300

    Added inheritance to new classes, menu items.

--- OO - Sabor-Express/app.py
+++ OO - Sabor-Express/app.py
@@ -1,20 +1,23 @@
 from modelos.restaurante import Restaurante
 '''Importa a classe Restaurante do arquivo restaurante.py, na pasta modelos.'''
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
+
 
 #Criando restaurantes:
 restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_praca.receber_avaliacao('Ninguém', 10)
-restaurante_praca.receber_avaliacao('Ulisses', 8)
-restaurante_praca.receber_avaliacao('Odisseu', 3)
-
 restaurante_mexicano = Restaurante('El Sombrero', 'Mexicana')
 restaurante_japones = Restaurante('Midorigoi','Japonesa')
+
+bebida_suco = Bebida('Suco de Melancia', 5.00, 'Grande')
+prato_paozinho = Prato('Pãozinho', 2.00, 'O melhor pão da cidade')
 
 restaurante_mexicano.alternar_estado()
 
 def main():
     '''Ao iniciar o programa, faz o seguinte:'''
-    Restaurante.listar_restaurantes()
+    print(bebida_suco)
+    print(prato_paozinho)
 
 if __name__ == '__main__':
     '''Se este arquivo não é chamado por outro, e sim aberto como programa principal,
